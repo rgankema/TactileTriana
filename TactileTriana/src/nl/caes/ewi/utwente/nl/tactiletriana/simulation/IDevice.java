49d87492d1a1c511828596e5c5f0204e20ad0fe7
Frumbasjt <richardgankema@gmail.com>
2015-03-09 10:02:01 +0100

    Added Interfaces and empty classes for simulation package

---
+++ TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/simulation/IDevice.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.caes.ewi.utwente.nl.tactiletriana.simulation;
+
+/**
+ *
+ * @author Richard
+ */
+public interface IDevice {
+    public void readyTick();
+    
+    public void tick();
+}
