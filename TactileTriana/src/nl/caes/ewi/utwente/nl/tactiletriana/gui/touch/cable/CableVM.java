c6fc27c2d75aa82ba6cd158229ba1c461100c085
Frumbasjt <richardgankema@gmail.com>
2015-03-10 13:23:17 +0100

    commit zodat ik verder kan op laptop

---
+++ TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/gui/touch/cable/CableVM.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.cable;
+
+import nl.caes.ewi.utwente.nl.tactiletriana.simulation.ICable;
+
+/**
+ *
+ * @author Richard
+ */
+public class CableVM {
+    private ICable model;
+    
+    public CableVM(ICable model) {
+        this.model = model;
+    }
+        
+}
