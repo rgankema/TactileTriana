0e2565da3373a501d849768107d236d74cc25016
Frumbasjt <richardgankema@gmail.com>
2015-03-10 15:36:47 +0100

    Cable ViewModel

--- TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/gui/touch/cable/CableVM.java
+++ TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/gui/touch/cable/CableVM.java
@@ -5,6 +5,10 @@
  */
 package nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.cable;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.beans.property.ReadOnlyDoubleWrapper;
 import nl.caes.ewi.utwente.nl.tactiletriana.simulation.ICable;
 
 /**
@@ -16,6 +20,21 @@ public class CableVM {
     
     public CableVM(ICable model) {
         this.model = model;
+       
+        Bindings.createDoubleBinding(() -> { return Math.min(1.0, Math.abs(model.getCurrent()) / model.getMaximumCurrent()); }, model.currentProperty(), model.maximumCurrentProperty());
+    }
+    
+    /**
+     * The load on the cable on a scale from 0 to 1.
+     */
+    private final ReadOnlyDoubleWrapper load = new ReadOnlyDoubleWrapper(0);
+    
+    public double getLoad() {
+        return load.get();
+    }
+    
+    public ReadOnlyDoubleProperty loadProperty() {
+        return load.getReadOnlyProperty();
     }
         
 }
