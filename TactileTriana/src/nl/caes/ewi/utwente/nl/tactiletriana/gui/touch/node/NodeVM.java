a332e01af8389da5e0d39bfd329db4531baaab5d
Frumbasjt <richardgankema@gmail.com>
2015-03-10 11:39:29 +0100

    Eerste poging tot View + ViewModel + Controller
    Waarbij View puur FXML is, ViewModel waar mogelijk de View beschrijft en
    Controller de koppeling tussen de twee verzorgt

--- TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/gui/touch/node/NodeVM.java
+++ TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/gui/touch/node/NodeVM.java
@@ -5,9 +5,14 @@
  */
 package nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.node;
 
+<<<<<<< HEAD
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.ReadOnlyDoubleWrapper;
+=======
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+>>>>>>> Eerste poging tot View + ViewModel + Controller
 import nl.caes.ewi.utwente.nl.tactiletriana.simulation.INode;
 
 /**
@@ -19,24 +24,28 @@ public class NodeVM {
     
     public NodeVM(INode model) {
         this.model = model;
-        
-        voltageErrorProperty.bind(Bindings.createDoubleBinding(() -> {
-            double difference = Math.abs(230 - this.model.getVoltage());
-            return Math.min(1.0, difference/23.0);
-        }, model.voltageProperty()));
+        this.model.voltageProperty().addListener(x -> {
+            double difference = Math.abs(230 - this.model.voltageProperty().get());
+            setVoltageError(Math.min(1.0, difference/23.0)); 
+        });
     }
     
     /**
-     * Defines how far off the voltage is in the node on a scale of 0 to 1.
-     * 0 means the voltage is at 230, 1 means it's off with at least 10%.
+     * Defines how far off the voltage is in the node from a scale of 0.0 to 1.0.
+     * 0.0 means the voltage is at 230, 1.0 means it's off with at least 10%.
      */    
-    private final ReadOnlyDoubleWrapper voltageErrorProperty = new ReadOnlyDoubleWrapper(0);
+    private final DoubleProperty voltageErrorProperty = new SimpleDoubleProperty(0);
     
     public double getVoltageError() {
         return voltageErrorProperty.get();
     }
     
-    public ReadOnlyDoubleProperty voltageErrorProperty() {
-        return voltageErrorProperty.getReadOnlyProperty();
+
+    private void setVoltageError(double value) {
+        voltageErrorProperty.set(value);
+    }
+    
+    public DoubleProperty voltageErrorProperty() {
+        return voltageErrorProperty;
     }
 }
