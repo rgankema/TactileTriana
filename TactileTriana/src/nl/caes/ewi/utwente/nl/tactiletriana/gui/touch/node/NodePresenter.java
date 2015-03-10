c04cea21f056fafd3b08f7dcbcb7388aaca36225
Frumbasjt <richardgankema@gmail.com>
2015-03-10 12:50:18 +0100

    Om de een of andere reden kan Git niets in een keer goed doen

---
+++ TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/gui/touch/node/NodePresenter.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.node;
+
+import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
+
+/**
+ * FXML Controller class
+ *
+ * @author Richard
+ */
+public class NodePresenter {
+    @FXML private Shape root;
+    
+    private NodeVM viewModel;
+    
+    public Shape getView() {
+        return root;
+    }
+    
+    /**
+     * Only to be called by factory
+     * @param viewModel 
+     */
+    protected void setViewModel(NodeVM viewModel) {
+        this.viewModel = viewModel;
+        
+        // Bind voltage error to color in view
+        viewModel.voltageErrorProperty().addListener(x -> {
+            double error = viewModel.getVoltageError();
+            root.setFill(new Color(error, 1.0 - error, 0, 1.0));
+        });
+        double error = viewModel.getVoltageError();
+        root.setFill(new Color(error, 1.0 - error, 0, 1.0));
+    }
+    
+}
