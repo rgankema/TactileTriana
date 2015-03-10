ce79669a5fad682bcff79bea1657274b726fc298
jd@leetbook <lala@fake>
2015-03-10 15:38:14 +0100

    Renamed IFBSweep to ISimulationInterface. Removed parent from cable.

--- TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/simulation/ISimulationNode.java
+++ TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/simulation/ISimulationNode.java
@@ -9,8 +9,8 @@ package nl.caes.ewi.utwente.nl.tactiletriana.simulation;
  *
  * @author jd
  */
-public interface IFBSweep {
+public interface ISimulationNode {
     
-    public double doForwardBackwardSweep(IFBSweep from, double v); 
+    public double doForwardBackwardSweep(ISimulationNode from, double v); 
     
 }
