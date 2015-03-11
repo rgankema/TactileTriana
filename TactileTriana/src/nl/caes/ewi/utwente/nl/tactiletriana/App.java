19da05cdcf070251946584af625a9ab76ffc6d18
Frumbasjt <richardgankema@gmail.com>
2015-03-11 14:12:05 +0100

    WEG MET PRESENTER :D
    Heb Model View ViewModel toch werkend gekregen denk ik. De dag is goed

--- TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/App.java
+++ TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/App.java
@@ -5,22 +5,10 @@
  */
 package nl.caes.ewi.utwente.nl.tactiletriana;
 
-import javafx.animation.Interpolator;
-import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-import nl.caes.ewi.utwente.nl.tactiletriana.gui.mock.MockCable;
-import nl.caes.ewi.utwente.nl.tactiletriana.gui.mock.MockNode;
-import nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.TouchPresenter;
-import nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.TouchPresenterFactory;
-import nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.cable.CablePresenterFactory;
-import nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.cable.CableVM;
-import nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.cable.DirectionView;
-import nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.node.NodePresenter;
-import nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.node.NodePresenterFactory;
-import nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.node.NodeVM;
+import nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.TouchView;
 
 /**
  *
@@ -30,14 +18,9 @@ public class App extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        TouchPresenterFactory factory = new TouchPresenterFactory();
-        TouchPresenter presenter = factory.buildTouchPresenter(null);
+        TouchView tv = new TouchView();
         
-        CablePresenterFactory f2 = new CablePresenterFactory();
-        
-        presenter.getView().getChildren().add(f2.buildCablePresenter(new CableVM(new MockCable())).getView());
-
-        Scene scene = new Scene(presenter.getView());
+        Scene scene = new Scene(tv);
         
         stage.setScene(scene);
         stage.setFullScreen(true);
