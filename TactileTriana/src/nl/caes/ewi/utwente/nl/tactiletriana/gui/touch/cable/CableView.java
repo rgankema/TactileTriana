d716df4be8496f2012dd7c0b3d3e16ba53b1a1af
Frumbasjt <richardgankema@gmail.com>
2015-03-12 11:01:34 +0100

    NetworkView af
    En daarmee de main touch view ook al voor een groot deel

--- TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/gui/touch/cable/CableView.java
+++ TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/gui/touch/cable/CableView.java
@@ -7,13 +7,14 @@ package nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.cable;
 
 import java.io.IOException;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
+import javafx.geometry.Bounds;
 import javafx.scene.Node;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
@@ -38,6 +39,8 @@ public class CableView extends Pane{
         } catch (IOException e) {
             throw new RuntimeException("Could not load CableView.fxml", e);
         }
+        
+        
     }
     
     public void setViewModel(CableVM viewModel) {
@@ -54,4 +57,130 @@ public class CableView extends Pane{
             return new Color(load, 1.0 - load, 0, 1.0);
         }, viewModel.loadProperty(), viewModel.brokenProperty()));
     }
+    
+    private final ObjectProperty<Node> startNode = new SimpleObjectProperty<Node>(null) {
+        @Override
+        public void set(Node value) {
+            if (value == get()) return;
+            
+            line.startXProperty().unbind();
+            line.startYProperty().unbind();
+            
+            if (value != null) {
+                line.startXProperty().bind(Bindings.createDoubleBinding(() -> {
+                    Bounds boundsInParent = value.getBoundsInParent();
+                    return boundsInParent.getMinX() + boundsInParent.getWidth() / 2;
+                }, value.boundsInParentProperty()));
+                line.startYProperty().bind(Bindings.createDoubleBinding(() -> {
+                    Bounds boundsInParent = value.getBoundsInParent();
+                    return boundsInParent.getMinY() + boundsInParent.getHeight() / 2;
+                }, value.boundsInParentProperty()));
+            }
+            super.set(value);
+        }
+    };
+    
+    public Node getStartNode() {
+        return startNode.get();
+    }
+    
+    public void setStartNode(Node startNode) {
+        this.startNode.set(startNode);
+    }
+    
+    public ObjectProperty<Node> startNodeProperty() {
+        return startNode;
+    }
+    
+    private final ObjectProperty<Node> endNode = new SimpleObjectProperty<Node>(null) {
+        @Override
+        public void set(Node value) {
+            if (value == get()) return;
+            
+            line.endXProperty().unbind();
+            line.endYProperty().unbind();
+            
+            if (value != null) {
+                line.endXProperty().bind(Bindings.createDoubleBinding(() -> {
+                    Bounds boundsInParent = value.getBoundsInParent();
+                    return boundsInParent.getMinX() + boundsInParent.getWidth() / 2;
+                }, value.boundsInParentProperty()));
+                line.endYProperty().bind(Bindings.createDoubleBinding(() -> {
+                    Bounds boundsInParent = value.getBoundsInParent();
+                    return boundsInParent.getMinY() + boundsInParent.getHeight() / 2;
+                }, value.boundsInParentProperty()));
+            }
+            super.set(value);
+        }
+    };
+    
+    public Node getEndNode() {
+        return endNode.get();
+    }
+    
+    public void setEndNode(Node endNode) {
+        this.endNode.set(endNode);
+    }
+    
+    public ObjectProperty<Node> endNodeProperty() {
+        return endNode;
+    }
+    
+    /*
+    private final DoubleProperty startX = new SimpleDoubleProperty(0);
+    
+    public double getStartX() {
+        return startX.get();
+    }
+    
+    public void setStartX(double startX) {
+        this.startX.set(startX);
+    }
+    
+    public DoubleProperty startXProperty() {
+        return startX;
+    }
+    
+    private final DoubleProperty startY = new SimpleDoubleProperty(0);
+    
+    public double getStartY() {
+        return startY.get();
+    }
+    
+    public void setStartY(double startY) {
+        this.startY.set(startY);
+    }
+    
+    public DoubleProperty startYProperty() {
+        return startY;
+    }
+    
+    private final DoubleProperty endX = new SimpleDoubleProperty(0);
+    
+    public double getEndX() {
+        return endX.get();
+    }
+    
+    public void setEndX(double endX) {
+        this.endX.set(endX);
+    }
+    
+    public DoubleProperty endXProperty() {
+        return endX;
+    }
+    
+    private final DoubleProperty endY = new SimpleDoubleProperty(0);
+    
+    public double getEndY() {
+        return endY.get();
+    }
+    
+    public void setEndY(double endY) {
+        this.endY.set(endY);
+    }
+    
+    public DoubleProperty endYProperty() {
+        return endY;
+    }
+    */
 }
---
+++ TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/gui/touch/transformer/TransformerView.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.transformer;
+
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.shape.Rectangle;
+
+/**
+ *
+ * @author Richard
+ */
+public class TransformerView extends Rectangle {
+    public TransformerView() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("TransformerView.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+        
+        try {
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load TransformerView.fxml", e);
+        }
+    }
+}
