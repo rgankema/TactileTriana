b6743782c5bbb6c7bff083c8c156366f14101773
Frumbasjt <richardgankema@gmail.com>
2015-03-12 11:53:08 +0100

    Begonnen aan DeviceView
    Roteren over symmetrielijn werkt al :D

--- TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/gui/touch/TouchView.java
+++ TactileTriana/src/nl/caes/ewi/utwente/nl/tactiletriana/gui/touch/TouchView.java
@@ -6,11 +6,13 @@
 package nl.caes.ewi.utwente.nl.tactiletriana.gui.touch;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.beans.binding.Bindings;
-import javafx.beans.property.DoubleProperty;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.AnchorPane;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
+import nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.device.DeviceView;
 import nl.caes.ewi.utwente.nl.tactiletriana.gui.touch.house.HouseView;
 import nl.utwente.cs.caes.tactile.control.TactilePane;
 
@@ -22,6 +24,7 @@ import nl.utwente.cs.caes.tactile.control.TactilePane;
 public class TouchView extends TactilePane {
     
     private TouchVM viewModel;
+    private List<DeviceView> devices;
     
     public TouchView() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("TouchView.fxml"));
@@ -32,6 +35,29 @@ public class TouchView extends TactilePane {
             loader.load();
         } catch (IOException e) {
             throw new RuntimeException("Could not load TouchView.fxml", e);
+        }
+        
+        // Create a stack of (dummy) devices
+        devices = new ArrayList<>();
+        double x = 1920/2 - 25;
+        double y = 1080/2 - 25;
+        for (int i = 0; i < 6; i++) {
+            DeviceView device = new DeviceView();
+            device.relocate(x, y);
+            
+            devices.add(device);
+            getChildren().add(device);
+        }
+        
+        for (Node node : getChildren()) {
+            if (node instanceof DeviceView) {
+                node.rotateProperty().bind(Bindings.createDoubleBinding(() -> {
+                    double rotate = node.getLayoutY() - y;
+                    if (rotate < -90) rotate = -90.0;
+                    if (rotate > 90) rotate = 90.0;
+                    return 90.0 - rotate;
+                }, node.layoutYProperty()));
+            }
         }
     }
     
