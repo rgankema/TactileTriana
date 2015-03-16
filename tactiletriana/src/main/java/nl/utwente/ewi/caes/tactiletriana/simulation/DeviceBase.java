d665974bf3f5e9a8593db113820482e9e473c125
Mick van der Vegt <mickvdv@gmail.com>
2015-03-16 13:41:32 +0100

    Properties geimplementeerd in DeviceBase

--- tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/simulation/DeviceBase.java
+++ tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/simulation/DeviceBase.java
@@ -6,8 +6,11 @@
 package nl.utwente.ewi.caes.tactiletriana.simulation;
 
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.beans.property.ReadOnlyDoubleWrapper;
 import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 
 /**
  *
@@ -60,28 +63,60 @@ public abstract class DeviceBase {
             this.minValue = minValue;
             this.maxValue = maxValue;
         }
+        
+        
     }
     
     /**
      * 
      * @return the amount of power that the device currently consumes
      */
-    public abstract ReadOnlyDoubleProperty currentConsumptionProperty();
+    public ReadOnlyDoubleProperty currentConsumptionProperty(){
+        return currentConsumptionWrapper;
+    }
+    
+    private ReadOnlyDoubleWrapper currentConsumptionWrapper = new ReadOnlyDoubleWrapper(0.0){
+        @Override
+        public void set(double value) {
+            // als hij disconnected is is hij altijd 0
+            if (getState() == DeviceBase.State.DISCONNECTED) {
+                value = 0;
+            }
+            super.set(value);
+        }
+    };
     
     public final double getCurrentConsumption() {
         return currentConsumptionProperty().get();
+    }
+    
+    public final void setCurrentConsumption(double value){
+        currentConsumptionWrapper.set(value);
     }
     
     /**
      * 
      * @return the state of this device
      */
-    public final ReadOnlyObjectProperty<State> stateProperty() {
-        throw new UnsupportedOperationException("Not supported yet");
+    private final ObjectProperty<State> state = new SimpleObjectProperty<State>(){
+        @Override
+        public void set(State value){
+            if (value == DeviceBase.State.DISCONNECTED){
+                setCurrentConsumption(0);
+            }
+            super.set(value);
+        }
+    };
+    public ObjectProperty<State> stateProperty() {
+        return this.state;
     }
     
     public final State getState() {
         return stateProperty().get();
+    }
+    
+    public final void setState(State s){
+        this.stateProperty().set(s);
     }
     
     /**
@@ -95,5 +130,9 @@ public abstract class DeviceBase {
      * for that tick, and updates it.
      * @param time  the amount of time that passed since the last tick
      */
-    public abstract void tick(double time);
+    public void tick(double time, boolean connected){
+        if(!connected){
+            this.setState(DeviceBase.State.DISCONNECTED);
+        }
+    }
 }
