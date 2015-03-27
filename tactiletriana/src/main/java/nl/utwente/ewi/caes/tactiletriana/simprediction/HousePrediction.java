962c438ab4b636739f51891b5093ce876fe1b879
Mick van der Vegt <mickvdv@gmail.com>
2015-03-27 15:20:51 +0100

    SimulationPrediction

---
+++ tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/simprediction/HousePrediction.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.utwente.ewi.caes.tactiletriana.simprediction;
+
+import nl.utwente.ewi.caes.tactiletriana.simulation.House;
+
+/**
+ *
+ * @author mickvdv
+ */
+public class HousePrediction extends House {
+    
+}
---
+++ tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/simprediction/SimulationPrediction.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.utwente.ewi.caes.tactiletriana.simprediction;
+
+import nl.utwente.ewi.caes.tactiletriana.simulation.Simulation;
+
+/**
+ *
+ * @author mickvdv
+ */
+public class SimulationPrediction extends Simulation {
+    private static SimulationPrediction instance;
+    
+    protected SimulationPrediction(){
+        super();
+        
+        // Koppelhuizen maken
+    }
+    
+    public static SimulationPrediction getInstance() {
+      if(instance == null) {
+         instance = new SimulationPrediction();
+      }
+      return instance;
+   }
+    
+}
