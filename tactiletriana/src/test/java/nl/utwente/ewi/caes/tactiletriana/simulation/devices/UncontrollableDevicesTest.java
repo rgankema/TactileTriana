d9ebba45620caf70685a39bb33c5cee2d4c1453c
jd@leetbook <lala@fake>
2015-04-09 16:00:31 +0200

    UncontrollableDevices: Added profile selection. Improved tests.

--- tactiletriana/src/test/java/nl/utwente/ewi/caes/tactiletriana/simulation/devices/UncontrollableDevicesTest.java
+++ tactiletriana/src/test/java/nl/utwente/ewi/caes/tactiletriana/simulation/devices/UncontrollableDevicesTest.java
@@ -29,8 +29,13 @@ public class UncontrollableDevicesTest {
         UncontrollableDevices uc = new UncontrollableDevices();
        
         ArrayList<Double> profile = uc.getProfile();
-        assertEquals(profile.get(0), 168d, 0.01);
-            
+        double value = profile.get(0);
+        assertTrue(value ==  168d || value ==  146d || value ==  240d || value ==  161d || value ==  230d || value ==  224d);
+        
+        UncontrollableDevices uc2 = new UncontrollableDevices(3);
+        double value2 = uc2.getProfile().get(0);
+        assertEquals(161d, value2, 0.01);
+        
     }
     
     public void testUncontrollableDevices() {
