035d7c52023844614c24021fb048f9f106e5d6cb
Mick van der Vegt <mickvdv@gmail.com>
2015-04-15 13:55:48 +0200

    House bind nu aan z'n spiegelhuis

--- tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/simprediction/HousePredictor.java
+++ tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/simprediction/HousePredictor.java
@@ -5,6 +5,11 @@
  */
 package nl.utwente.ewi.caes.tactiletriana.simprediction;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.ListChangeListener;
 import nl.utwente.ewi.caes.tactiletriana.simulation.DeviceBase;
 import nl.utwente.ewi.caes.tactiletriana.simulation.House;
@@ -23,9 +28,12 @@ public class HousePredictor extends House {
      * @param linkedHouse House waar deze HousePrediciton aan gekoppeld is.
      * @param simulation
      */
+    Map<DeviceBase, DeviceBase> shadowDeviceMap;
+
     public HousePredictor(House linkedHouse, Simulation simulation) {
         super(simulation);
         this.linkedHouse = linkedHouse;
+        shadowDeviceMap = new HashMap<DeviceBase, DeviceBase>();
 
         this.linkedHouse.getDevices().addListener(new ListChangeListener<DeviceBase>() {
 
@@ -39,11 +47,30 @@ public class HousePredictor extends House {
                     } else if (c.wasUpdated()) {
                         //update item
                     } else {
-                        for (DeviceBase remitem : c.getRemoved()) {
+                        for (DeviceBase item : c.getRemoved()) {
                             //remitem.remove(Outer.this);
+                            getDevices().remove(shadowDeviceMap.get(item));
                         }
-                        for (DeviceBase additem : c.getAddedSubList()) {
-                            //additem.add(Outer.this);
+                        for (DeviceBase item : c.getAddedSubList()) {
+                            // maak een kopie van dit device in de map
+                            DeviceBase newDevice = null;
+                            try {
+                                //additem.add(Outer.this);
+                                newDevice = item.getClass().asSubclass(item.getClass()).getConstructor(simulation.getClass()).newInstance(simulation);
+                            } catch (Exception ex) {
+                                System.out.println("House Predictor is stuk");
+                            }
+                            
+                            // sla het nieuwe device op in de map
+                            shadowDeviceMap.put(item, newDevice);
+                            
+                            // bind alle parameters
+                            for (int i = 0; i < item.getParameters().size(); i++){
+                                newDevice.getParameters().get(i).property.bind(item.getParameters().get(i).property);
+                            }
+                            
+                            // voeg het toe aan dit huis
+                            getDevices().add(newDevice);
                         }
                     }
                 }
@@ -52,5 +79,3 @@ public class HousePredictor extends House {
 
     }
 }
-
-
