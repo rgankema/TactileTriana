4c2bacc65e99c30f786f9ee2eff1f10435317629
Niels ten Boom <Nielstenboom@gmail.com>
2015-04-23 17:36:35 +0200

    Few additions to BufferTimeShiftable

--- tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/simulation/devices/BufferTimeShiftable.java
+++ tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/simulation/devices/BufferTimeShiftable.java
@@ -6,6 +6,7 @@
 package nl.utwente.ewi.caes.tactiletriana.simulation.devices;
 
 import java.time.LocalDateTime;
+import nl.utwente.ewi.caes.tactiletriana.SimulationConfig;
 import nl.utwente.ewi.caes.tactiletriana.simulation.DeviceBase;
 import nl.utwente.ewi.caes.tactiletriana.simulation.Simulation;
 
@@ -14,7 +15,11 @@ import nl.utwente.ewi.caes.tactiletriana.simulation.Simulation;
  * @author niels
  */
 public class BufferTimeShiftable extends DeviceBase{
-
+    
+    //Capacity in W/h
+    private double capacity = 85000; //FIXME add other car models (currently Tesla)
+    private double currentCharge = 0;
+    
     public BufferTimeShiftable(Simulation simulation) {
         super(simulation, EntityType.BUFFER_TIME_SHIFTABLE);
     }
@@ -26,17 +31,38 @@ public class BufferTimeShiftable extends DeviceBase{
 
         //Set the current consumption according to current temperature, radiation and time
         setCurrentConsumption(getCurrentConsumption(simulation.getCurrentTime()));
+        System.out.println(currentCharge);
     }
+    
     
     public double getCurrentConsumption(LocalDateTime date){
         int h = date.getHour();
         double result = 0;
-        if ((0 <= h && h <= 8) || ( 18 <= h && h <= 23)){
-            result = 1000;
+        if (((0 <= h && h <= 8) || ( 18 <= h && h <= 23)) && !isCharged()){
+            result = 7500;
+            chargeBuffer(result,SimulationConfig.SIMULATION_TICK_TIME);
+        //Drain battery during day
+        } else if (!(0 <= h && h <= 8) || ( 18 <= h && h <= 23)){
+            chargeBuffer(-7500,SimulationConfig.SIMULATION_TICK_TIME);
         }
         return result;
     }
     
     
+    public void chargeBuffer(double W, double timestep){
+        if (!isCharged()){
+            currentCharge = currentCharge + W*(timestep/60);
+        }
+        //Make sure charge doesn't exceed capacity and doesn't get below zero
+        if (currentCharge > capacity){
+            currentCharge = capacity;
+        } else if (currentCharge < 0){
+            currentCharge = 0;
+        }
+    }
     
+    public boolean isCharged(){
+        return currentCharge == capacity;
+    }
+            
 }
