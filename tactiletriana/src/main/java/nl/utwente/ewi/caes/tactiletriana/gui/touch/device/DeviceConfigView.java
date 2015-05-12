dad2d28d9aba95f6172fd90bc0daa16a4cc7f2de
Frumbasjt <richardgankema@gmail.com>
2015-05-12 21:32:48 +0200

    asdasd

--- tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/gui/touch/device/DeviceConfigView.java
+++ tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/gui/touch/device/DeviceConfigView.java
@@ -5,17 +5,20 @@
  */
 package nl.utwente.ewi.caes.tactiletriana.gui.touch.device;
 
-import java.util.List;
+import javafx.beans.property.DoubleProperty;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.input.TouchEvent;
 import javafx.scene.layout.GridPane;
 import nl.utwente.ewi.caes.tactiletriana.gui.ViewLoader;
 import nl.utwente.ewi.caes.tactiletriana.gui.customcontrols.Carousel;
-import nl.utwente.ewi.caes.tactiletriana.simulation.DeviceBase.ConfigurableCategory;
-import nl.utwente.ewi.caes.tactiletriana.simulation.DeviceBase.ConfigurableDouble;
-import nl.utwente.ewi.caes.tactiletriana.simulation.DeviceBase.Configurable;
+import nl.utwente.ewi.caes.tactiletriana.simulation.DeviceBase;
+import nl.utwente.ewi.caes.tactiletriana.simulation.devices.Buffer;
+import nl.utwente.ewi.caes.tactiletriana.simulation.devices.ElectricVehicle;
+import nl.utwente.ewi.caes.tactiletriana.simulation.devices.SolarPanel;
+import nl.utwente.ewi.caes.tactiletriana.simulation.devices.TimeShiftableBase;
 
 /**
  * A panel for configuring device parameters. Has two columns, with labels on
@@ -26,34 +29,51 @@ import nl.utwente.ewi.caes.tactiletriana.simulation.DeviceBase.Configurable;
 class DeviceConfigView extends GridPane {
     @FXML private Label header;
     
-    public DeviceConfigView(String header, List<Configurable> parameters) {
+    public DeviceConfigView(DeviceBase device) {
         ViewLoader.load(this);
         
-        this.header.setText(header);
+        this.header.setText(device.getDisplayName());
         
-        int row = 1;
-        for (Configurable p : parameters) {
-            this.add(new Label(p.getDisplayName()), 0, row);
-            
-            if (p instanceof ConfigurableDouble) {
-                ConfigurableDouble dp = (ConfigurableDouble)p;
-                Slider s = new Slider(dp.getMin(), dp.getMax(), dp.getProperty().get());
-
-                // consume touch events so that the deviceview can't be dragged while using the slider
-                // todo: fix TactilePane so that this isn't necessary anymore
-                s.addEventFilter(TouchEvent.ANY, e -> e.consume());
-
-                p.getProperty().bindBidirectional(s.valueProperty());
-                this.add(s, 1, row);
-            } else { // p instanceof CategoryParameter
-                ConfigurableCategory cp = (ConfigurableCategory)p;
-                Carousel c = new Carousel(cp.getProperty(), o -> cp.getCurrentValueDisplayName(), cp.getPossibleValues());
-                
-                this.add(c, 1, row);
-            }
-            row++;
+        if (device instanceof Buffer) {     // Buffer
+            Buffer buffer = (Buffer) device;
+            Slider capacity = buildSlider(5000, 20000, buffer.capacityProperty());
+            Slider maxPower = buildSlider(500, 3700, buffer.maxPowerProperty());
+            addControl("Capacity", capacity);
+            addControl("Max Power", maxPower);
+        } else if (device instanceof TimeShiftableBase) {   // WashingMachine and Dishwasher
+            TimeShiftableBase timeShiftable = (TimeShiftableBase) device;
+            Slider startTime = buildSlider(0, 24*60 - 1, timeShiftable.startTimeProperty());
+            Slider delay = buildSlider(timeShiftable.getProgram().length, 24*60 - timeShiftable.getProgram().length, timeShiftable.delayProperty());
+            addControl("Start Time", startTime);
+            addControl("Delay", delay);
+        } else if (device instanceof SolarPanel) {  // SolarPanel
+            SolarPanel solarPanel = (SolarPanel) device;
+            Slider area = buildSlider(2, 40, solarPanel.areaProperty());
+            addControl("Area", area);
+        } else if (device instanceof ElectricVehicle) { // EV
+            ElectricVehicle electricVehicle = (ElectricVehicle) device;
+            Carousel model = new Carousel(electricVehicle.modelProperty(), ev -> electricVehicle.getModelName(), ElectricVehicle.Model.values());
+            addControl("Model", model);
         }
+    }
+    
+    private Slider buildSlider(double min, double max, DoubleProperty property) {
+        Slider result = new Slider();
+        result.setMin(min);
+        result.setMax(max);
+        result.valueProperty().bindBidirectional(property);
         
+        // consume touch events so that the deviceview can't be dragged while using the slider
+        // todo: fix TactilePane so that this isn't necessary anymore
+        result.addEventFilter(TouchEvent.ANY, e -> e.consume());
         
+        return result;
+    }
+    
+    private int row = 1;
+    private void addControl(String label, Node control) {
+        this.add(new Label(label), 0, row);
+        this.add(control, 1, row);
+        row++;
     }
 }
