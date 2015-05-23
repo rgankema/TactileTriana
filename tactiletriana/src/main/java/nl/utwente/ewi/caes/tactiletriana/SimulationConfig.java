124553516eddc61baf58562e44b9e403e268771c
Frumbasjt <richardgankema@gmail.com>
2015-05-23 16:41:21 +0200

    min_date max_date uit config gehaald, hoort daar helemaal niet

--- tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/SimulationConfig.java
+++ tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/SimulationConfig.java
@@ -8,7 +8,6 @@ package nl.utwente.ewi.caes.tactiletriana;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.Properties;
 
 /**
@@ -25,9 +24,6 @@ public final class SimulationConfig {
     public static int SYSTEM_TICK_TIME;       // time between ticks in ms
     public static boolean SIMULATION_UNCONTROLABLE_LOAD_ENABLED; // staat de uncontrolable load aan?
     public static int HOUSE_MAX_FUSE_CURRENT;
-    
-    public static LocalDate MIN_DATE;
-    public static LocalDate MAX_DATE; 
 
     public static String LoadProperty(String title) throws IOException {
         // load the property
@@ -42,8 +38,6 @@ public final class SimulationConfig {
             SYSTEM_TICK_TIME = Integer.parseInt(LoadProperty("SYSTEM_TICK_TIME"));
             SIMULATION_UNCONTROLABLE_LOAD_ENABLED = Boolean.parseBoolean(LoadProperty("SIMULATION_UNCONTROLABLE_LOAD_ENABLED"));
             HOUSE_MAX_FUSE_CURRENT = Integer.parseInt(LoadProperty("HOUSE_MAX_FUSE_CURRENT"));
-            MAX_DATE = LocalDate.parse(LoadProperty("MAX_DATE"));
-            MIN_DATE = LocalDate.parse(LoadProperty("MIN_DATE"));
         }
         catch (Exception e){
             System.out.println("Error: Could not read the config file.");
@@ -51,8 +45,6 @@ public final class SimulationConfig {
             SYSTEM_TICK_TIME = 200;
             SIMULATION_UNCONTROLABLE_LOAD_ENABLED = true;
             HOUSE_MAX_FUSE_CURRENT = 3 * 35;
-            MAX_DATE = LocalDate.of(2014, 12, 31);
-            MIN_DATE = LocalDate.of(2014, 1, 1);
         }        
     }
     public static void SaveProperties(){
@@ -60,8 +52,6 @@ public final class SimulationConfig {
         SaveProperty("SYSTEM_TICK_TIME", String.valueOf(SYSTEM_TICK_TIME));
         SaveProperty("SIMULATION_UNCONTROLABLE_LOAD_ENABLED", String.valueOf(SIMULATION_UNCONTROLABLE_LOAD_ENABLED));
         SaveProperty("HOUSE_MAX_FUSE_CURRENT", String.valueOf(HOUSE_MAX_FUSE_CURRENT));
-        SaveProperty("MAX_DATE", MAX_DATE.toString());
-        SaveProperty("MIN_DATE", MIN_DATE.toString());
     }
 
     public static void SaveProperty(String title, String value) {
