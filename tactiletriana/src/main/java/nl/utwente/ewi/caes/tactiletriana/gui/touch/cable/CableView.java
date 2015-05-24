82ded00a197ac14ee8176377124d1e665ba1337e
Frumbasjt <richardgankema@gmail.com>
2015-05-24 11:10:32 +0200

    Eerste versie geanimeerde kabels

--- tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/gui/touch/cable/CableView.java
+++ tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/gui/touch/cable/CableView.java
@@ -5,18 +5,23 @@
  */
 package nl.utwente.ewi.caes.tactiletriana.gui.touch.cable;
 
+import java.util.ArrayList;
+import java.util.List;
+import javafx.animation.AnimationTimer;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.DoubleBinding;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.geometry.Bounds;
+import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Glow;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
-import nl.utwente.ewi.caes.tactiletriana.gui.StageController;
 import nl.utwente.ewi.caes.tactiletriana.gui.ViewLoader;
 import nl.utwente.ewi.caes.tactiletriana.gui.events.EventUtil;
 
@@ -83,7 +88,7 @@ public class CableView extends Group {
         // Bind diameter of cables to direction in viewmodel
         DoubleBinding diameterBinding = Bindings.createDoubleBinding(() -> {
             double current = viewModel.getMaximumCurrent();
-            // Ik ga er voor het gemak vanuit dat de maximum current zich 1 op 1 verhoudt met de diameter van de kabel, later checken of dat klopt
+            // Ik ga er voor het gemak vanuit dat de maximum current zich 1 op 1 verhoudt met de diameter van de kabel
             double diameter = 2 * Math.sqrt(current / Math.PI);
             return diameter;
         }, viewModel.loadProperty());
@@ -95,8 +100,11 @@ public class CableView extends Group {
         directionEnd.scaleYProperty().bind(diameterBinding.divide(15d));
 
         // Bind visibility of direction views to direction in viewmodel
-        directionStart.visibleProperty().bind(viewModel.directionProperty().isEqualTo(CableVM.Direction.START));
-        directionEnd.visibleProperty().bind(viewModel.directionProperty().isEqualTo(CableVM.Direction.END));
+        directionEnd.setVisible(false);
+        directionStart.setVisible(false);
+        
+        //directionStart.visibleProperty().bind(viewModel.directionProperty().isEqualTo(CableVM.Direction.START));
+        //directionEnd.visibleProperty().bind(viewModel.directionProperty().isEqualTo(CableVM.Direction.END));
 
         // Repair cable on short press, show on chart for long press
         EventUtil.addShortAndLongPressEventHandler(line, l -> {
@@ -112,8 +120,93 @@ public class CableView extends Group {
                 line.setEffect(null);
             }
         });
+        
+        animate();
     }
 
+    private void animate() {
+        final List<Circle> circles = new ArrayList<>();
+        final List<Circle> removed = new ArrayList<>();
+        AnimationTimer timer = new AnimationTimer() {
+            long last = -1;
+            
+            @Override
+            public void handle(long now) {
+                if (viewModel.getLoad() == 0) {
+                    for (Circle circle : circles) {
+                        removed.add(circle);
+                        getChildren().remove(circle);
+                    }
+                    for (Circle circle : removed) {
+                        circles.remove(circle);
+                    }   
+                    return;
+                }
+                if (last == -1) {
+                    last = now;
+                    return;
+                } else {
+                    if ((now - last) / 1000000 >= (3000 - 2950 * viewModel.getLoad())) {
+                        Circle circle;
+                        if (removed.size() > 0) {
+                            circle = removed.get(0);
+                            circle.setTranslateX(0);
+                            circle.setTranslateY(0);
+                            removed.remove(0);
+                        } else {
+                            circle = new Circle(line.getStrokeWidth() * 0.3);
+                            circle.layoutXProperty().bind(Bindings.createDoubleBinding(() -> { 
+                                return (viewModel.getDirection() == CableVM.Direction.END) ? line.getStartX() : line.getEndX();
+                            }, viewModel.directionProperty(), line.startXProperty(), line.endXProperty()));
+                            circle.layoutYProperty().bind(Bindings.createDoubleBinding(() -> { 
+                                return (viewModel.getDirection() == CableVM.Direction.END) ? line.getStartY() : line.getEndY();
+                            }, viewModel.directionProperty(), line.startYProperty(), line.endYProperty()));
+                            viewModel.directionProperty().addListener((obs, oldV, newV) -> { 
+                                circle.setTranslateX(0 - circle.getTranslateX());
+                                circle.setTranslateY(0 - circle.getTranslateY());
+                            });
+                            
+                            circle.setFill(Color.YELLOW);
+                            circle.setEffect(new Glow(0.7));
+                        }
+                        getChildren().add(circle);
+                        circles.add(circle);
+                        
+                        last = now;
+                    }
+                }
+                for (Circle circle : circles) {
+                    double speed = 1.5 + 3.0 * viewModel.getLoad();
+
+                    Point2D end;
+                    if (viewModel.getDirection() == CableVM.Direction.END) {
+                        end = new Point2D(line.getEndX() - line.getStartX(), line.getEndY() - line.getStartY());
+                    } else {
+                        end = new Point2D(line.getStartX() - line.getEndX(), line.getStartY() - line.getEndY());
+                    }
+                    Point2D current = new Point2D(circle.getTranslateX(), circle.getTranslateY());
+
+                    Point2D delta = end.subtract(current);
+                    if (delta.magnitude() < 2.0) {
+                        removed.add(circle);
+                        getChildren().remove(circle);
+                        continue;
+                    }
+
+                    Point2D direction = delta.normalize();
+
+                    circle.setTranslateX(circle.getTranslateX() + direction.getX() * speed);
+                    circle.setTranslateY(circle.getTranslateY() + direction.getY() * speed);
+                }
+                for (Circle circle : removed) {
+                    circles.remove(circle);
+                }
+            }
+        };
+        
+        timer.start();
+    }
+    
     // PROPERTIES
     /**
      * The JavaFX Node that defines where the cable starts.
