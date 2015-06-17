8b95a643bee23acb0dcdca8f2f00f00e48951d79
JH Kuipers <jd@leetbook>
2015-06-17 13:30:41 +0200

    Added enable/disable functionality for the API.

--- tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/simulation/Simulation.java
+++ tactiletriana/src/main/java/nl/utwente/ewi/caes/tactiletriana/simulation/Simulation.java
@@ -16,6 +16,8 @@ import javafx.beans.property.SimpleObjectProperty;
 import nl.utwente.ewi.caes.tactiletriana.Concurrent;
 import static nl.utwente.ewi.caes.tactiletriana.Concurrent.runOnJavaFXThreadSynchronously;
 import nl.utwente.ewi.caes.tactiletriana.GlobalSettings;
+import nl.utwente.ewi.caes.tactiletriana.simulation.devices.BufferBase;
+import nl.utwente.ewi.caes.tactiletriana.simulation.devices.TimeShiftableBase;
 import nl.utwente.ewi.caes.tactiletriana.simulation.devices.UncontrollableLoad;
 
 /**
@@ -25,10 +27,12 @@ import nl.utwente.ewi.caes.tactiletriana.simulation.devices.UncontrollableLoad;
 public class Simulation extends SimulationBase {
 
     private static final TimeScenario DEFAULT_SCENARIO = new TimeScenario(new TimeScenario.TimeSpan(LocalDate.of(2014, 1, 1), LocalDate.of(2014, 12, 31)));
-
+    
+    private boolean apiEnabled;
+    
     public Simulation() {
         this.setState(SimulationState.STOPPED);
-
+        this.apiEnabled = true;
         // Initialise time
         setCurrentTime(getTimeScenario().getCurrentTime());
     }
@@ -158,11 +162,44 @@ public class Simulation extends SimulationBase {
             incrementTime();
         });
 
-        if (getController() != null) {
+        if (getController() != null && apiEnabled) {
             getController().retrievePlanning(50, getCurrentTime());
         }
     }
-
+    
+    
+    /** 
+     * Returns if the Simulation uses the API to request plannings or not
+     * 
+     * @return boolean indicating if the API is enabled or not 
+     */
+    public boolean getAPIState() {
+        return this.apiEnabled;
+    }
+    
+    /**
+     * Enable the requests for plannings by the API
+     */
+    public void enableAPI() {
+        this.apiEnabled = true;
+    }
+    
+    /**
+     * Prevent the API from requesting plannings and clear all current plannings 
+     */
+    public void disableAPI() {
+        this.apiEnabled = false;
+        //Clear all currently set plannings 
+        for(DeviceBase device : this.getDevices()) {
+            if (device instanceof BufferBase) {
+                ((BufferBase)device).getPlanning().clear();
+            } else if (device instanceof TimeShiftableBase) {
+                ((TimeShiftableBase)device).getPlanning().clear();
+            }
+        }
+        
+    }
+    
     /**
      * Increments the time by either the tick time, or leaps to the next time
      * span if required.
